3418c2fa44811f384fc4b8d25e8d0cac70fec7f1
Mikita Tsybulka <nikita.tsybulka@gmail.com>
2025-12-12 09:43:54 +0100

    [added] interrupts

---
+++ Interrupts/task1/main.s
@@ -0,0 +1,24 @@
+.data
+	string: .ascii "Hello, world!"
+	end_string:
+	
+	.set string_size, end_string - string
+	
+
+.text
+	.globl _start
+
+_start:
+	mov $4, %eax
+	mov $1, %ebx
+	mov $string, %ecx
+	mov $string_size, %edx
+	int $0x80
+
+__begin:
+	
+
+__end:
+	mov $1, %eax 
+	mov $0, %ebx
+	int $0x80
